4b6c22002115954bb54bc84560bf2d320e54a6b3
supportzrto <support.zrto@gmail.com>
2026-06-05 10:49:51 +0530

    add reminder api

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,3 +4,4 @@ from app.models.task import Task
 from app.models.task_update import TaskUpdate
 from app.models.reminder import Reminder
 from app.models.telegram_state import TelegramState
+from app.models.notification import Notification
